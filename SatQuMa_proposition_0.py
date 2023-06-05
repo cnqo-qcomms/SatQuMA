3de2830bb95c63ada3b7c154036b04cfd1df95c7
Sch <s.gressani@protonmail.ch>
2023-06-05 13:09:17 +0200

    Modified the parameters and add the ranges as implemented in satQuma

--- SatQuMa_proposition_0.py
+++ SatQuMa_proposition_0.py
@@ -38,7 +38,7 @@ class Key_Rate_Calculator():
                  intensisty_1_selection_probability : float,
                  intensisty_2_selection_probability : float,
                  x_basis_selection_probability : float,
-                 receiver_efficiency, 
+                 detector_efficiency, 
                  dark_count_probability, 
                  after_pulse_probability,
                  error_rate_due_to_optical_errors):
@@ -75,7 +75,7 @@ class Key_Rate_Calculator():
         self.x_basis_selection_probability = x_basis_selection_probability
         self.z_basis_selection_probability = self.get_basis_selection_probability("z")
         
-        self.receiver_efficiency = receiver_efficiency
+        self.detector_efficiency = detector_efficiency
         self.dark_count_probability = dark_count_probability
         self.after_pulse_probability = after_pulse_probability
         self.error_rate_due_to_optical_errors = error_rate_due_to_optical_errors
@@ -100,13 +100,12 @@ class Key_Rate_Calculator():
     
     def get_system_efficiency(self):
         "η_sys = η_ch*η_bob"
-        system_efficiency = self.channel_efficiency*self.receiver_efficiency
+        system_efficiency = self.channel_efficiency*self.detector_efficiency
         return system_efficiency
     
     def get_expecting_detection_rate_excluding_after_pulse(self, intensity):
         "D_k = 1 - (1 - 2*p_dc)*np.exp(-η_sys*k)"
-        expecting_detection_rate_excluding_after_pulse = (1 - 
-                                                         (1 - 2*self.dark_count_probability)*np.exp(-self.get_system_efficiency()*intensity))
+        expecting_detection_rate_excluding_after_pulse = (1 - (1 - 2*self.dark_count_probability)*np.exp(-self.get_system_efficiency()*intensity))
 
         return expecting_detection_rate_excluding_after_pulse
 
@@ -118,13 +117,13 @@ class Key_Rate_Calculator():
     def get_bit_error_probability(self, intensity):
         "e_k = p_dc + e_mis*(1 - np.exp(-η_ch*k)) + 0.5*p_ap*D_k"
         bit_error_probability = (self.dark_count_probability
-                                 + self.error_rate_due_to_optical_errors*(1 - np.exp(-self.channel_efficiency*intensity))
-                                 + 0.5*self.after_pulse_probability*self.get_expecting_detection_rate_excluding_after_pulse(intensity))
+                                 + self.error_rate_due_to_optical_errors*(1 - np.exp(-self.get_system_efficiency()*intensity))
+                                 + 0.5*self.after_pulse_probability*self.get_expecting_detection_rate_including_after_pulse(intensity))
         return bit_error_probability
     
     def get_number_of_events(self, intensity, basis):
         "n_j_k = ..."
-        number_of_events = self.get_basis_selection_probability(basis)*self.get_basis_selection_probability(basis)*self.number_of_pulses*self.get_intensity_selection_probability(intensity)*self.get_expecting_detection_rate_excluding_after_pulse(intensity)
+        number_of_events = self.get_basis_selection_probability(basis)*self.get_basis_selection_probability(basis)*self.number_of_pulses*self.get_intensity_selection_probability(intensity)*self.get_expecting_detection_rate_including_after_pulse(intensity)
         return number_of_events
     
     def get_post_processing_block_size(self, basis):
@@ -148,7 +147,7 @@ class Key_Rate_Calculator():
     
     def get_number_of_errors(self, intensity, basis):
         "m_j_k = ..."
-        number_of_errors = self.get_bit_error_probability(intensity)*self.get_number_of_events(intensity, basis)/self.get_expecting_detection_rate_excluding_after_pulse(intensity)
+        number_of_errors = self.get_bit_error_probability(intensity)*self.get_number_of_events(intensity, basis)/self.get_expecting_detection_rate_including_after_pulse(intensity)
         return number_of_errors
     
     def get_post_processing_block_size_2(self, basis): # A renommer
@@ -184,38 +183,45 @@ class Key_Rate_Calculator():
                                                     + (np.exp(-self.intensity_3)*self.get_intensity_selection_probability(self.intensity_3)*self.intensity_3**n))/np.math.factorial(n)
         return n_photon_state_transmission_probability
     
-    def get_appoximate_number_of_vacuum_events(self, basis):
+    def get_approximate_number_of_vacuum_events(self, basis):
         "s_j_0 = ..."
         numerator = (self.intensity_2*self.get_number_of_events_lowerbound(self.intensity_3, basis)
                       - self.intensity_3*self.get_number_of_events_upperbound(self.intensity_2, basis))*self.get_n_photon_state_transmission_probability(0)
         denominator = self.intensity_2 - self.intensity_3
-        appoximate_number_of_vacuum_events = numerator/denominator
-        return appoximate_number_of_vacuum_events
+        approximate_number_of_vacuum_events = max(numerator/denominator, 1e-10)
+        return approximate_number_of_vacuum_events
     
-    def get_appoximate_number_of_single_photon_events(self, basis):
+    def get_approximate_number_of_single_photon_events(self, basis):
         "s_j_1 = ..."
         numerator = self.get_n_photon_state_transmission_probability(1)*self.intensity_1*(self.get_number_of_events_lowerbound(self.intensity_2, basis)
                                                                                           - self.get_number_of_events_upperbound(self.intensity_3, basis)
                                                                                           - (self.intensity_2**2 - self.intensity_3**2)*(self.get_number_of_events_upperbound(self.intensity_1, basis) 
-                                                                                                                                          - self.get_appoximate_number_of_vacuum_events(basis)/self.get_n_photon_state_transmission_probability(0))/self.intensity_1**2)
+                                                                                                                                          - self.get_approximate_number_of_vacuum_events(basis)/self.get_n_photon_state_transmission_probability(0))/self.intensity_1**2)
         denominator = self.intensity_1*(self.intensity_2 - self.intensity_3) - self.intensity_2**2 + self.intensity_3**2
-        appoximate_number_of_single_photon_events = numerator/denominator
-        return appoximate_number_of_single_photon_events
+        approximate_number_of_single_photon_events = max(numerator/denominator, 1e-10)
+        return approximate_number_of_single_photon_events
     
     def get_approximate_number_of_bit_error(self, basis):
         "v_j_1 = ..."
         numerator = self.get_n_photon_state_transmission_probability(1)*(self.get_number_of_errors_upperbound(self.intensity_2, basis) - self.get_number_of_errors_lowerbound(self.intensity_3, basis))
         denominator = self.intensity_2 - self.intensity_3
-        approximate_number_of_bit_error = numerator/denominator
+        approximate_number_of_bit_error = min(max(numerator/denominator, 1e-10), 
+                                              self.get_post_processing_block_size_2("z"))
         return approximate_number_of_bit_error
     
     def get_phase_error_rate(self):
         "φ_x = ..."
-        phase_error_rate = (self.get_approximate_number_of_bit_error("z")/self.get_appoximate_number_of_single_photon_events("z")
+        from sys import float_info
+        # Extract the smallest float that the current system can:
+        num_min = float_info.epsilon
+        ratio = min(self.get_approximate_number_of_bit_error("z")/self.get_approximate_number_of_single_photon_events("z"), 
+                     num_min)
+        phase_error_rate = (self.get_approximate_number_of_bit_error("z")/self.get_approximate_number_of_single_photon_events("z")
                             + gamma(self.secrecy_error,
-                                    self.get_approximate_number_of_bit_error("z")/self.get_appoximate_number_of_single_photon_events("z"),
-                                    self.get_appoximate_number_of_single_photon_events("z"),
-                                    self.get_appoximate_number_of_single_photon_events("x")))
+                                   ratio,
+                                    self.get_approximate_number_of_single_photon_events("z"),
+                                    self.get_approximate_number_of_single_photon_events("x")))
+        phase_error_rate = min(phase_error_rate, 0.5)
         return phase_error_rate
     
     def get_estimated_number_of_bits_for_error_correction(self, basis):
@@ -225,61 +231,45 @@ class Key_Rate_Calculator():
     
     def get_secret_key_length(self):
         "l = ..."
-        secret_key_length = (self.get_appoximate_number_of_vacuum_events("x")
-                              + self.get_appoximate_number_of_single_photon_events("x")
-                              - self.get_appoximate_number_of_single_photon_events("x")*get_binary_entropy(self.get_phase_error_rate())
+        secret_key_length = (self.get_approximate_number_of_vacuum_events("x")
+                              + self.get_approximate_number_of_single_photon_events("x")
+                              - self.get_approximate_number_of_single_photon_events("x")*get_binary_entropy(self.get_phase_error_rate())
                               - self.get_estimated_number_of_bits_for_error_correction("x")
                               - 6*np.log2(21/self.secrecy_error)
                               - np.log2(2/self.correctness_error))
+        secret_key_length = max(secret_key_length, 0)
         return secret_key_length
     
 
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
-krc = Key_Rate_Calculator(channel_efficiency = 0.000194,
-                          number_of_pulses = 1e9,
-                          secrecy_error = 1e-9,
-                          correctness_error = 1e-15,
-                          average_observed_error_rate_in_x = 10,
-                          error_correction_efficiency = 1.16,
-                          intensity_1 = 0.7921,
-                          intensity_2 = 0.1707,
-                          intensity_3 = 0,
-                          intensisty_1_selection_probability = 0.7501,
-                          intensisty_2_selection_probability = 0.1749,
-                          x_basis_selection_probability = 0.7611,
-                          receiver_efficiency = 1.0, 
-                          dark_count_probability = 1.00e-08, 
-                          after_pulse_probability = 0.001,
-                          error_rate_due_to_optical_errors = 0.001)
-krc.get_secret_key_length()
         
-# x = defaultdict(list) 
-# y = defaultdict(list) 
-# for s in range(4, 10):     
-#     for l in list(range(201)):
-#         krc = Key_Rate_Calculator(channel_efficiency = get_channel_efficiency(l),
-#                                   number_of_pulses = 10**s,
-#                                   secrecy_error = 1e-9,
-#                                   correctness_error = 1e-15,
-#                                   average_observed_error_rate_in_x = 10,
-#                                   error_correction_efficiency = 1.16,
-#                                   intensity_1 = 0.7921,
-#                                   intensity_2 = 0.1707,
-#                                   intensity_3 = 0,
-#                                   intensisty_1_selection_probability = 0.7501,
-#                                   intensisty_2_selection_probability = 0.1749,
-#                                   x_basis_selection_probability = 0.7611,
-#                                   receiver_efficiency = 1.0, 
-#                                   dark_count_probability = 1.00e-08, 
-#                                   after_pulse_probability = 0.001,
-#                                   error_rate_due_to_optical_errors = 0.001)
-#         skr = krc.get_secret_key_length()
-#         if (not np.isnan(skr) and skr > 0):
-#             x[s].append(l)
-#             y[s].append(krc.get_secret_key_length())
-#     plt.semilogy(x[s],  y[s])
+x = defaultdict(list) 
+y = defaultdict(list) 
+for s in range(4, 10):     
+    for l in list(range(201)):
+        krc = Key_Rate_Calculator(channel_efficiency = get_channel_efficiency(l),
+                                  number_of_pulses = 10**s,
+                                  secrecy_error = 1e-9,
+                                  correctness_error = 1e-15,
+                                  average_observed_error_rate_in_x = 10,
+                                  error_correction_efficiency = 1.16,
+                                  intensity_1 = 0.7921,
+                                  intensity_2 = 0.1707,
+                                  intensity_3 = 0,
+                                  intensisty_1_selection_probability = 0.7501,
+                                  intensisty_2_selection_probability = 0.1749,
+                                  x_basis_selection_probability = 0.7611,
+                                  detector_efficiency = 1.0, 
+                                  dark_count_probability = 1.00e-08, 
+                                  after_pulse_probability = 0.001,
+                                  error_rate_due_to_optical_errors = 0.001)
+        skr = krc.get_secret_key_length()
+        if (not np.isnan(skr) and skr > 0):
+            x[s].append(l)
+            y[s].append(krc.get_secret_key_length())
+    plt.semilogy(x[s],  y[s])
 
     
   
